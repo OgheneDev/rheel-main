7126dbe2ea34da399d2e5ed3e138794df3bd8f3d
OgheneDev <emmanueloghene72@gmail.com>
2025-03-15 11:59:01 +0100

    loading

--- app/components/property/StateComponents.tsx
+++ app/components/property/StateComponents.tsx
@@ -1,3 +1,7 @@
-export const LoadingState = () => <p>Loading property details...</p>;
+export const LoadingState = () =>  <div className="loader-container">
+<div className="flex items-center justify-center w-full h-[700px]">
+  <div className="spinner-border animate-spin inline-block w-10 h-10 border-4 rounded-full text-[#0A2F1E]"></div>
+</div>
+</div>;
 
 export const ErrorState = () => <p>Property not found.</p>;
