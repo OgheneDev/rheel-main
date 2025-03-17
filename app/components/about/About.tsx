a0086016d59e7523918123e6d3d8e5a12e6b76c5
OgheneDev <emmanueloghene72@gmail.com>
2025-03-17 18:35:14 +0100

    remove image

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -62,19 +62,12 @@ const About = () => {
                 <li>Accountability – We take full responsibility for every transaction, providing accurate, well-documented records and keeping our clients informed throughout.</li>
             </ul>
         </article>
-        <div className='flex items-start flex-col md:flex-row mb-5'>
+        <div className='flex items-start md:justify-center flex-col md:flex-row mb-5'>
             <Image
              alt="Core Values"
              src='/images/values.png'
              height={200}
              width={500}
-            />
-            <Image
-             alt="About"
-             src='/images/about.png'
-             height={100}
-             width={500}
-             className="h-[380px]"
             />
         </div>
         <div className="text-center mb-5">
