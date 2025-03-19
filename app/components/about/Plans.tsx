3601506922cf4fd33ae900f03d5c8206e8874139
OgheneDev <emmanueloghene72@gmail.com>
2025-03-19 16:34:31 +0100

    about us page

--- app/components/about/Plans.tsx
+++ app/components/about/Plans.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Plans = () => {
   return (
-    <div className='pb-8'>
+    <div className='pb-8 md:px-8'>
         <h3 className='text-xl font-bold uppercase mb-2'>Our Plans</h3>
         <div className="space-y-5">
             <p className='leading-7.5'>Rheel Estate is more than a business; it’s a commitment to changing the real estate landscape in Nigeria. As we continue to grow, we remain focused on improving the experience of every client we serve, and we aim to expand our footprint across Nigeria, creating a better, more accessible property market for all. We believe that our commitment to quality, ethical practices, and transparency will help build stronger communities, more satisfied homeowners, and a thriving real estate sector for the future.</p>
